fa13ede758ac947e7dea0e39cb4bf6f6fba4d3ee
Lun Sovathana <sovathanalun@gmail.com>
2015-11-20 14:48:52 +0700

    Initial Commit

---
+++ Currency Request/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Currency Request
+//
+//  Created by Lun Sovathana on 11/20/15.
+//  Copyright © 2015 Lun Sovathana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Currency RequestTests/Currency_RequestTests.m
@@ -0,0 +1,39 @@
+//
+//  Currency_RequestTests.m
+//  Currency RequestTests
+//
+//  Created by Lun Sovathana on 11/20/15.
+//  Copyright © 2015 Lun Sovathana. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Currency_RequestTests : XCTestCase
+
+@end
+
+@implementation Currency_RequestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
